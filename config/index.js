9331b19708ce7f9f46e6dbb0c99a7851d267480c
theresajvrensburg <theresa@isogramconsulting.com>
2025-04-12 08:09:25 +0200

    test

--- config/index.js
+++ config/index.js
@@ -22,13 +22,8 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  app.use(
-    cors({
-      origin: ['*', FRONTEND_URL]
-    })
-  );
+  app.use(cors());
 
-  app.options("*", cors());
 
   // In development environment the app logs
   app.use(logger("dev"));
